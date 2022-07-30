193f3affa1e08c16f5f95b43c9ae5e5fe56c41e4
AnnaHuskova <nebopole@ukr.net>
2022-07-30 20:15:36 +0300

    slider design

--- main.js
+++ main.js
@@ -60,3 +60,20 @@ window.addEventListener('load', function(){
     })
 
 });
+
+
+// -------------Slider-Bar-------------
+var slides = document.getElementsByClassName("slide");
+var index = 0;
+function goLeft() {
+  if(index == 0) index = slides.length - 1;
+  else index--;
+
+  slides[0].style.marginLeft = "-" + index * 1200 + "px";
+}
+function goRight() {
+  if(index == slides.length -1) index = 0;
+  else index++;
+
+  slides[0].style.marginLeft = "-" + index * 1200 + "px";
+}
